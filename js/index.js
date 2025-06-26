9d00b15a46ae184da042d0ae9cf5a054027c58fa
ggvictor <victorrodriguessilva2@gmail.com>
2025-06-26 18:22:36 -0300

    avançando

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@
 const buttons = document.querySelectorAll(".button")
 const paises = document.querySelector("#paises")
 const futebol = document.querySelector("#futebol")
+const filmes = document.querySelector("#filmes")
+const mangas = document.querySelector("#mangas")
 
 // funções
 function delay(){
@@ -19,14 +21,23 @@ buttons.forEach(button => {
 });
 
 paises.addEventListener('click', () => {
-    localStorage.setItem("modo1", "paises")
-    localStorage.setItem("imagem1", "img/paises.png")
+    localStorage.setItem("modo", "Paises")
+    localStorage.setItem("imagem", "img/paises.png")
     window.location.href = "game.html"
 })
 
 futebol.addEventListener('click', () => {
-    localStorage.setItem("modo2", "futebol")
-    localStorage.setItem("imagem2", "img/futebol.png")
+    localStorage.setItem("modo", "Futebol")
+    localStorage.setItem("imagem", "img/futebol.png")
     window.location.href = "game.html"
 })
-
+filmes.addEventListener('click', () => {
+    localStorage.setItem("modo", "Filmes")
+    localStorage.setItem("imagem", "img/filmes.png")
+    window.location.href = "game.html"
+})
+mangas.addEventListener('click', () => {
+    localStorage.setItem("modo", "Mangas")
+    localStorage.setItem("imagem", "img/mangas.png")
+    window.location.href = "game.html"
+})
--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 // variaveis
 let icon = document.getElementById("icon")
 let nameGame = document.querySelector(".game-name")
+const input = document.querySelectorAll(".letter")
+
+for(let linha of input){
+    linha.maxLength = 1;
+}
 // funções 
 const change = ()=>{
     nameGame.innerHTML = "Paises"
@@ -9,20 +14,15 @@ const change = ()=>{
 
 
 window.addEventListener("DOMContentLoaded", () => {
-    const modo1 = localStorage.getItem("modo1")
-    const imagem1 = localStorage.getItem("imagem1")
+    const modo = localStorage.getItem("modo");
+    const imagem = localStorage.getItem("imagem");
 
-    if (modo1 && nameGame) {
-        nameGame.innerHTML = modo1
-    }
-    if(imagem1 && icon){
-        icon.src = imagem1
-    }
+    if (modo && imagem) {
+        nameGame.innerHTML = modo;
+        icon.src = imagem;
 
-    const modo2 = localStorage.getItem("modo2")
-    const imagem2 = localStorage.getItem(imagem2)
-
-    if(modo2 && nameGame){
-        nameGame.innerHTML = modo2
+        // limpa os dados após usar (opcional, mas ajuda a evitar conflitos)
+        localStorage.removeItem("modo");
+        localStorage.removeItem("imagem");
     }
-})
+});
